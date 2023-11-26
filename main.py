a19a2685218037d5b2aa29c6a8bfef1fefc3cc87
Scott Sattler <scott.sattler.dev@gmail.com>
2023-11-26 15:07:40 -0600

    updates

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+import pydantic
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
